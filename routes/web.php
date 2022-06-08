0366d03b5ba1a01273585976420f0c87e8c8d550
Alexxboc <boccardi.alessandro@gmail.com>
2022-06-08 15:52:09 +0200

    Create dynamic menu in home page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $menuList = ['Home', 'About', 'Contacts', 'More info', 'Game'];
+    return view('home', compact('menuList'));
 });
